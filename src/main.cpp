e6b056bdee286d0873ce48faa7c83607ec0dc1cb
Benjamin Wagner <bewagner@uni-bremen.de>
2020-04-25 18:59:26 +0200

    Changed stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <opencv4/opencv2/opencv.hpp>
-//#include <opencv2/opencv.hpp>
+#include "opencv2/opencv.hpp"
+
 
 int main() {
     std::cout << "Hello world" << std::endl;
