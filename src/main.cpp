0f8809877e95b65a9d9cd9898929274b76118923
Benjamin Wagner <bewagner@uni-bremen.de>
2020-04-25 17:03:27 +0200

    Delete building opencv in travis file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <opencv4/opencv2/opencv.hpp>
+//#include <opencv2/opencv.hpp>
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
+    std::cout << "Hello world" << std::endl;
     std::cout << "Opencv version is " << cv::getVersionString() << std::endl;
 }
