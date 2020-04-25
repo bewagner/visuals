57ec72445c80ce52423614d8065371931592bf42
Benjamin Wagner <bewagner@uni-bremen.de>
2020-04-25 11:27:01 +0200

    Link against opencv

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
 
 int main() {
     std::cout << "Hello world!" << std::endl;
+    std::cout << "Opencv version is " << cv::getVersionString() << std::endl;
 }
